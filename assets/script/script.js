62d510bcf6eeff0815c73c4928c1f83eab986d7f
Nicholas Herold <nherold86@gmail.com>
2021-06-07 21:56:12 -0400

    created functions to pull in restraunts based on zip or geolocation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,39 @@
+let zip = "";
 
+// Function finds list of reaurants in the area based on lat and lon
+function Getrestaurants(lat,lon) {
+    fetch('https://api.documenu.com/v2/restaurants/search/geo?key=5162cc5a0a88bba9f4483c32d07d87f7&lat='+ lat +'&lon='+lon+'&distance=1&fullmenu')
+    .then(response => {
+      return response.json();  
+    })
+    .then (data =>{
+        console.log(data);
+    });
+}
+
+function Ziprestaurants(zipcode) {
+    fetch('https://api.documenu.com/v2/restaurants/zip_code/'+zipcode+'?key=5162cc5a0a88bba9f4483c32d07d87f7&size=10')
+    .then(response => {
+      return response.json();  
+    })
+    .then (data =>{
+        console.log(data);
+    });
+}
+
+
+function success(pos) {
+    let latitude = pos.coords.latitude
+    console.log(latitude)
+    let longitude = pos.coords.longitude
+    console.log(longitude)  
+    Getrestaurants(latitude,longitude)  
+}
+
+function error(){
+    // display zipcode search if location not granted
+    zip = '53094'
+    Ziprestaurants(zip)
+}
+
+navigator.geolocation.getCurrentPosition(success, error)
